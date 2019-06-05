bee0512fd7183f2ce86bfe53018cf5683acf30e5
Henrique de Castilhos <henrique.castilhos@softexpert.com>
2019-06-05 10:27:08 -0300

    :wrench: Public path alterado

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/second-degree-equation/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/simpsons-rule/' : '/'
 }
