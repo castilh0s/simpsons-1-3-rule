f8ebb5655dcb462f55ace60ad66e77abe2eddda1
Henrique de Castilhos <henrique.castilhos@softexpert.com>
2019-06-05 10:29:49 -0300

    :art: Algoritmo para a regra de 1/3 de Simpson

--- src/assets/simpsons-rule.js
+++ src/assets/simpsons-rule.js
@@ -1,72 +1,29 @@
-function SecondDegreeEquation (valueA, valueB, valueC) {
-  this.valueA = valueA
-  this.valueB = valueB
-  this.valueC = valueC
+function func (x) {
+  return Math.log(x)
 }
 
-SecondDegreeEquation.prototype = {
-  calculateDelta () {
-    return Math.pow(this.valueB, 2) - 4 * this.valueA * this.valueC
-  },
-  calculateX () {
-    let delta = this.calculateDelta()
-    let squareDelta = Math.sqrt(delta)
-    let result = {}
+function simpson (lower, upper, n) {
+  let h = (upper - lower) / n
+  let x = []
+  let fx = []
 
-    if (delta < 0) {
-      result.error = true
-      result.errorMessage = 'Não há raízes reais.'
+  for (let index = 0; index <= n; index++) {
+    x[index] = lower + index * h
+    fx[index] = func(x[index])
+  }
+
+  let res = 0
+  for (let index = 0; index <= n; index++) {
+    if (index === 0 || index === n) {
+      res += fx[index]
+    } else if (index % 2 !== 0) {
+      res += 4 * fx[index]
     } else {
-      result.x1 = (-this.valueB + squareDelta) / (2 * this.valueA)
-      if (delta > 0) {
-        result.x2 = (-this.valueB - squareDelta) / (2 * this.valueA)
-      }
-    }
-
-    return result
-  },
-  calculateVertex () {
-    let XVertex = (this.valueB / (2 * this.valueA)) * (-1)
-    let YVertex = (this.calculateDelta() / (4 * this.valueA)) * (-1)
-
-    return {
-      x: XVertex,
-      y: YVertex
-    }
-  },
-  getImage () {
-    if (this.valueA > 0) {
-      return '{ y ∈ R / y ≥ Yv }'
-    } else if (this.valueA < 0) {
-      return '{ y ∈ R / y ≤ Yv }'
-    } else {
-      return {
-        error: true,
-        errorMessage: 'Valor de A não pode ser zero.'
-      }
-    }
-  },
-  calculate () {
-    let xValues = this.calculateX()
-    let vertexValues = this.calculateVertex()
-    let image = this.getImage()
-
-    if (xValues.error) {
-      return xValues
-    }
-
-    if (image.error) {
-      return image
-    }
-
-    return {
-      xValues: xValues,
-      vertexValues: vertexValues,
-      image: image
+      res += 2 * fx[index]
     }
   }
+
+  return res * (h / 3)
 }
 
-module.exports = {
-  SecondDegreeEquation
-}
+console.log(simpson(4, 5.2, 6))
